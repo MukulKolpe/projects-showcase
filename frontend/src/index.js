5334a21ad0c35626270d443bee954abe79c57ae2
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-04-01 10:39:11 +0530

    chore:card component and modal done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
